83a9c7bf695269cd6c0fc9a31436a9c1ff1f9853
ortizfrancielly <ortizfrancielly@gmail.com>
2022-01-14 18:06:21 -0300

    criar projeto inicial no xcode

---
+++ MiniChallenge-5/MiniChallenge-5/MiniChallenge_5App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge_5App.swift
+//  MiniChallenge-5
+//
+//  Created by Francielly Cristina Ortiz Candido on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
